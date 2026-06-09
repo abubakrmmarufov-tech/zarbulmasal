a6cc68558c65032cf653af50b49f89959cd22e13
ma'rufi <abubakrmmarufov@gmail.com>
2026-06-10 05:08:11 +0800

    Final release web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1104108640" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4077297595" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
