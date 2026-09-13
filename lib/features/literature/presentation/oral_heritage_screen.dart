6ccfb26bca34b30cd2436a3059350b877a539124
ma'rufi <237553621+abubakrmmarufov-tech@users.noreply.github.com>
2026-09-14 02:28:06 +0800

    fix: format literature imports

--- lib/features/literature/presentation/oral_heritage_screen.dart
+++ lib/features/literature/presentation/oral_heritage_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../../core/design_system/design_system.dart';
 import '../../../core/l10n/app_translations.dart';
 import '../../../shared/providers/app_providers.dart';
