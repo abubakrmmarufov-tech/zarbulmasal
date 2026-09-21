f3dc0eb10ee71a03609a61d993f441887914ac37
ma'rufi <abubakrmmarufov@gmail.com>
2026-09-22 02:41:43 +0800

    Deploy audited version with CSP fix

--- qalam_service_worker.js
+++ qalam_service_worker.js
@@ -7,7 +7,7 @@ const CACHE_NAMESPACE = 'zarbulmasal-offline:';
 const LEGACY_CACHE_NAMESPACE = 'zarbulmasal-offline-v1:';
 const WORKER_URL = new URL(self.location.href);
 const BUILD_ID = WORKER_URL.searchParams.get('build') ||
-  '__ZARBULMASAL_BUILD_ID__';
+  '99cf033ec580f1b1a520';
 const CANVASKIT_VARIANT = WORKER_URL.searchParams.get('variant') === 'chromium'
   ? 'chromium'
   : 'full';
